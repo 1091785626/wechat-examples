f7eaebd0fafdcd88c487c886a4ef165103572f8e
Deot <zrd0921@qq.com>
2017-01-06 15:39:05 +0800

    跑通流程
    fixed

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -31,11 +31,6 @@ const pageConfig = {
 			};
 			this.actions.request(url, params, {});
 		}
-	},
-	onReady(){
-		setTimeout(()=>{
-			this.$noticeStart();
-		},5000);
 	}
 };
 const combineConfig = Object.assign({},diyConfig,skuConfig,toastConfig,routeConfig,pageConfig);
